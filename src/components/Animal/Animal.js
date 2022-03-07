cff503ce06152fc142ada7ca0d374b204631a97b
Denver McCarthy <dmichaelspdx@gmail.com>
2022-03-07 13:00:52 -0800

    animal images rendering to page

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,5 +1,9 @@
 import React from 'react';
+export default function Animal(props) {
 
-export default function Animal() {
-  return <></>;
+  return (<div className='animal' style={{ top: `${props.top}`, left: `${props.left}` }}>
+    <img alt={props.name} src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`}/>
+    <span className='name'>{props.name}</span>
+    <span>{props.says}</span>
+  </div>);
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import Animal from '../Animal/Animal';
+import { animals } from '../../data.js';
 import './Main.css';
 
 export default function Main() {
-  return <main></main>;
+  return (<main style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/background.png)` }}>
+    {animals.map((animal) => (
+      <Animal key={animal.id} type={animal.type} name={animal.name} says={animal.says} top={animal.top} left={animal.left} />
+    ))}
+  </main>);
 }
